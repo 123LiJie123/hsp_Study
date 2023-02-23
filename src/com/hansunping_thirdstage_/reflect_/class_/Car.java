b19d9748a6e355182ed1d176a9a23ba22ad6e415
123LiJie123 <1145485072@qq.com>
2023-02-23 16:51:08 +0800

    上传

--- src/com/hansunping_thirdstage_/reflect_/class_/Car.java
+++ src/com/hansunping_thirdstage_/reflect_/class_/Car.java
@@ -1,8 +1,40 @@
 package com.hansunping_thirdstage_.reflect_.class_;
 
 class Car {
-    public String color;
-    public String brand;
-    public int price;
+    public String color = "白色";
+    public String brand = "宝马";
+    public int price = 500000;
 
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "color='" + color + '\'' +
+                ", brand='" + brand + '\'' +
+                ", price=" + price +
+                '}';
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
 }
--- src/com/hansunping_thirdstage_/reflect_/class_/Class02.java
+++ src/com/hansunping_thirdstage_/reflect_/class_/Class02.java
@@ -1,4 +1,68 @@
 package com.hansunping_thirdstage_.reflect_.class_;
 
+import java.lang.reflect.Field;
+
+/**
+ * 演示Class类的常用方法
+ */
 public class Class02 {
+    public static void main(String[] args) {
+        String classAllPath = "com.hansunping_thirdstage_.reflect_.class_.Car";
+        //获取到Car类对应的Class对象
+
+        try {
+
+
+            //1、获取到Car类对应的Class对象
+            //<?>表示不确定的Java类型
+            Class<?> carClass = Class.forName(classAllPath);
+
+
+            //2、输出carClass
+            //显示carClass对象，是哪个类的Class对象 com.hspedu.Car
+            System.out.println(carClass);
+            //输出carClass运行类型 java.lang.Class
+            System.out.println(carClass.getClass());
+
+
+            //3、得到包名
+            System.out.println(carClass.getPackage().getName());
+
+
+            //4、得到全类名
+            System.out.println(carClass.getName());
+
+
+            //5、通过carClass创建对象实例
+            Car car = (Car) carClass.newInstance();
+            System.out.println(car);
+
+
+            //6、 通过反射获取属性brand,只能获取公有属性
+            Field brand = carClass.getField("brand");
+            System.out.println(brand.get(car));
+
+            //7、通过反射给属性赋值
+            brand.set(car, "奔驰");
+            System.out.println(brand.get(car));
+
+
+            //8、通过carClass得到car中所有属性
+            Field[] fields = carClass.getFields();
+            for (int i = 0; i < fields.length; i++) {
+                System.out.println(fields[i].get(car));
+            }
+
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
