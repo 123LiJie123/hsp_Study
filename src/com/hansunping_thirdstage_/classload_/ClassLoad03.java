b95d35b0c135906fb67d26ac4a43cb93b52b7177
123LiJie123 <1145485072@qq.com>
2023-03-01 14:27:25 +0800

    上传

--- src/com/hansunping_thirdstage_/classload_/ClassLoad03.java
+++ src/com/hansunping_thirdstage_/classload_/ClassLoad03.java
@@ -1,16 +1,26 @@
 package com.hansunping_thirdstage_.classload_;
 
 public class ClassLoad03 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException {
 
         //1、加载B类，并生成B的class对象
         //2、连接 num = 0
         //3、初始化阶段
         // 依次自动收集类中的所有静态变量的赋值动作和静态代码块中的语句,并合并
 
-        new B();
+        System.out.println(B.num);
+
+        B b = new B();
+        System.out.println(b.num);
+        // new B(); //类加载
+        // System.out.println(B.num);  //100，如果直接使用类的静态属性，也会导致类的加载
+
+        //加载类的时候，是有同步机制控制的
+
+
     }
 }
+
 
 class B {
     static {
